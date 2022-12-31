59520e0b9d7f0456a107e5564042f29bf141575a
Fullstack-Developer-Bhabashree <108857510+Fullstack-Developer-Bhabashree@users.noreply.github.com>
2023-01-01 00:52:25 +0530

    Update DetailsComponent.js
    Made changes to the axios method and implemented directly th e object returned in the response

--- src/components/DetailsComponent.js
+++ src/components/DetailsComponent.js
@@ -11,42 +11,28 @@ export function DetailsComponent() {
     const params = useParams();
 
     useEffect(() => {
-        const options = {
-            method: 'GET',
-            url: `${BASE_URL}/getProductDetails/${params.id}`
-        }
-
-        axios.request(options).then((response) => {
-            setProduct([]);
-            console.log(response.data)
-            setProduct(response.data);
-        }).catch((err) => {
-            console.error(err);
+        axios.get(`${BASE_URL}/getProductDetails/${params.id}`).then(async (response) => {
+            await setProduct([])
+            console.log(response.data[0]);
+            await setProduct(response.data[0])
         })
-    }, [])
+    }, [params.id])
 
     return (
         <div>
             <h2>Product Details</h2>
-
-            {
-                product.map((item) => {
-                    return (
                         <dl className="row">
                             <dt className="col-3">Product Id :</dt>
-                            <dd className="col-9">{item.id}</dd>
+                            <dd className="col-9">{product.id}</dd>
                             <dt className="col-12">Preview :</dt>
-                            <dd className="col-12"><img src={item.image} width="100" height="100" className="border border-4 p-2" /></dd>
+                            <dd className="col-12"><img src={product.image} width="100" height="100" className="border border-4 p-2" /></dd>
                             <dt className="col-3">Product Title :</dt>
-                            <dd className="col-9">{item.title}</dd>
+                            <dd className="col-9">{product.title}</dd>
                             <dt className="col-3">Description :</dt>
-                            <dd className="col-9">{item.description}</dd>
+                            <dd className="col-9">{product.description}</dd>
                             <dt className="col-3">Price:</dt>
-                            <dd className="col-9"><b>${item.price}/-</b></dd>
+                            <dd className="col-9"><b>${product.price}/-</b></dd>
                         </dl>
-                    )
-                })
-            }
             <Link to={'/products/' + product.category}><span className="bi bi-chevron-double-left">Back to Products</span></Link>
         </div>
     )
